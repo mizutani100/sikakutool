d8f973306a8806cdb5db33741954a8f193d37c45
sikaku00 <110365446+mizutani100@users.noreply.github.com>
2022-08-07 12:31:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('アレルギー発見ツール')
 st.caption('これはアレルギー成分を成分表から見つけるツールです')
-ale = st.text_input("指定する成分") #引数に入力内容を渡せる
+ale = st.text_input("見つけたい成分") #引数に入力内容を渡せる
 zen = st.text_area('全成分')  #引数に入力内容を渡せる
 
 Button = st.button("実行")
